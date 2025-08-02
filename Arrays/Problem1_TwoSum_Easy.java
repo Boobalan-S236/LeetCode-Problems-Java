710da2fb66061a6b701e040ddb7a213884878a15
Boobalan-S236 <sboobalan2005@gmail.com>
2025-08-03 00:13:48 +0530

    Update Problem1_TwoSum_Easy.java

--- Arrays/Problem1_TwoSum_Easy.java
+++ Arrays/Problem1_TwoSum_Easy.java
@@ -1,3 +1,7 @@
+// Problem: 1. Two Sum
+// Link: https://leetcode.com/problems/two-sum/
+// Difficulty: Easy
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
