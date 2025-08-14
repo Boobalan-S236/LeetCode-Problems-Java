26032950d28ce5b81d2232ee0078eb0c01debc52
Boobalan-S236 <sboobalan2005@gmail.com>
2025-08-14 22:46:24 +0530

    744. Find Smallest Letter Greater Than Target

---
+++ Arrays/Problem740_Binary_Search.java
@@ -0,0 +1,20 @@
+
+// Problem: 704. Binary Search
+// Link: https://leetcode.com/problems/binary-search/
+// Difficulty: Easy
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int start=0,end=nums.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }else end=mid-1;
+        }
+        return -1;
+    }
+}
---
+++ Binary/Problem744_Find_Smallest_Letter_Greater_Than_Target.java
@@ -0,0 +1,20 @@
+
+// Problem: 744. Find Smallest Letter Greater Than Target
+// Link: https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+// Difficulty:Easy
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start=0,end=letters.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target < letters[mid]){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return letters[start%letters.length];
+    }
+}
