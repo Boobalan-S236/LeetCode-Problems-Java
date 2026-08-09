eb414cfcc385c2ec6a9d5e11fce0d6cf93f36e48
Boobalan-S236 <sboobalan462@gmail.com>
2026-08-09 21:12:33 +0530

    Time: 20 ms (83.42%), Space: 56.4 MB (57.67%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
-        
+
         int i=0,j=people.length-1,boat=0;
         while(i<=j){
             int sum =people[i]+people[j];
@@ -10,15 +10,15 @@ class Solution {
                 i++;j--;
             }
             else if(people[i]<people[j]){
-                if(people[j]<=limit){
-                    boat++;
-                }
+                // if(people[j]<=limit){
+                     boat++;
+                // }
                 j--;
             }
             else{
-                if(people[i]<limit){
-                    boat++;
-                }
+                // if(people[i]<limit){
+                     boat++;
+                // }
                 i++;
             }
         }
