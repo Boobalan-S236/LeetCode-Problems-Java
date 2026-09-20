366e84f8a00dfca2993892d7d32aff2268966a1f
Boobalan-S236 <sboobalan462@gmail.com>
2026-09-20 16:11:12 +0530

    Time: 1 ms (96.27%), Space: 47.4 MB (40.65%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int rs=0,ls=0,ts=0;
+        for(int i=0;i<nums.length;i++){
+            ts+=nums[i];
+        }
+        rs=ts;
+        for(int i=0;i<nums.length;i++){
+            rs-=nums[i];
+            if(rs == ls){
+                return i;
+            }
+            ls+=nums[i];
+        }
+
+        return -1;
+    }
+}
